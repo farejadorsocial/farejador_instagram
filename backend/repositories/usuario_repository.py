5be19a9debf19bad11e1524a36b2daa9907a4f47
farejadorsocial <yhagoreis2@gmail.com>
2026-09-04 21:39:50 -0300

    Organizar repository de usuários após migração para PostgreSQL

--- backend/repositories/usuario_repository.py
+++ backend/repositories/usuario_repository.py
@@ -1,7 +1,21 @@
 from backend.core import auth
 
-def preparar_usuarios(): return auth.preparar_usuarios()
-def register(username, password, dados_acesso=None): return auth.register(username, password, dados_acesso)
-def login(username, password, dados_acesso=None): return auth.login(username, password, dados_acesso)
-def logout(token): return auth.logout(token)
-def get_user(token): return auth.get_user(token)
+
+def preparar_usuarios():
+    return auth.preparar_usuarios()
+
+
+def register(username, password, dados_acesso=None):
+    return auth.register(username, password, dados_acesso)
+
+
+def login(username, password, dados_acesso=None):
+    return auth.login(username, password, dados_acesso)
+
+
+def logout(token):
+    return auth.logout(token)
+
+
+def get_user(token):
+    return auth.get_user(token)
