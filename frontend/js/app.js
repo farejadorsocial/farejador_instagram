e546ad0baabb1bed5e2683fd675bf7085a43ac73
farejadorsocial <yhagoreis2@gmail.com>
2026-09-06 00:19:10 -0300

    fix: renovar cache dos módulos de explorar

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -5,7 +5,7 @@
 (function carregarModulos(lista, indice) {
   if (indice >= lista.length) return;
   const script = document.createElement("script");
-  script.src = `${lista[indice]}?v=20260905-category2`;
+  script.src = `${lista[indice]}?v=20260906-category3`;
   script.onload = function () { carregarModulos(lista, indice + 1); };
   script.onerror = function () {
     console.error("Falha ao carregar módulo:", lista[indice]);
