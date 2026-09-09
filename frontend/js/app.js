5bd94f2d867830ad3a034495ba79011e93205640
farejadorsocial <yhagoreis2@gmail.com>
2026-09-09 11:39:37 -0300

    chore: atualizar cache dos módulos da interface

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -5,7 +5,7 @@
 (function carregarModulos(lista, indice) {
   if (indice >= lista.length) return;
   const script = document.createElement("script");
-  script.src = `${lista[indice]}?v=20260906-ranking4`;
+  script.src = `${lista[indice]}?v=20260909-credits1`;
   script.onload = function () { carregarModulos(lista, indice + 1); };
   script.onerror = function () {
     console.error("Falha ao carregar módulo:", lista[indice]);
